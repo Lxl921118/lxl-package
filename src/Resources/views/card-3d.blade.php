e9d676bcd68bd8c3f2e90ec6f82ec1ff1f74b5d8
Lxl <asd22251299@gmail.com>
2026-01-08 02:32:04 +0000

    [feat] 元件屬性測試

--- src/Resources/views/card-3d.blade.php
+++ src/Resources/views/card-3d.blade.php
@@ -1,7 +1,7 @@
 <div class="hover-3d">
   <!-- content -->
   <figure class="max-w-100 rounded-2xl">
-    <img src="https://img.daisyui.com/images/stock/creditcard.webp" alt="3D card" />
+    <img src="{{ $imgSrc ?? 'https://img.daisyui.com/images/stock/creditcard.webp' }}" alt="3D card" />
   </figure>
   <!-- 8 empty divs needed for the 3D effect -->
   <div></div>
