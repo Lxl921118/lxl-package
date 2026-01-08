0579d6a7bdb0511f22a3ddec548910f3417fa380
Lxl <asd22251299@gmail.com>
2026-01-08 05:20:24 +0000

    [refactor] 調整 Button 元件的屬性順序，修正顯示類型與文字的註解

--- src/Resources/views/components/button.blade.php
+++ src/Resources/views/components/button.blade.php
@@ -1,9 +1,14 @@
 {{-- resources/views/components/button.blade.php --}}
 @props([
     /**
-     * @var string Button 顯示文字
+     * @var string Button 顯示類型
      */
     'type' => 'button || submit || reset',
+
+    /**
+     * @var string Button 顯示文字
+     */
+    'text' => '按鈕',
 ])
 
 <button type="{{ $type }}">{{ $text }}</button>
